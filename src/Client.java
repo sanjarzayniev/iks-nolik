f51657725416124b73d9eca4a55808e6f24080c2
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2023-11-30 23:43:40 +0500

    Remove unused modules, line

--- src/Client.java
+++ src/Client.java
@@ -4,7 +4,6 @@ import frames.LoginFrame;
 public class Client {
     public static void main(String[] args) throws Exception {
         GameFrame gameFrame = new GameFrame();
-        gameFrame.setVisible(false);
         LoginFrame loginFrame = new LoginFrame(gameFrame);
         loginFrame.setVisible(true);
     }
